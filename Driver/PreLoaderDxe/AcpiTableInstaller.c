9da0957a4fd04d3b1b44687b381b3c24d074b128
Gustave Monce <gustave.monce@outlook.com>
2020-06-23 18:40:25 +0200

    Add preloader header

--- Driver/PreLoaderDxe/AcpiTableInstaller.c
+++ Driver/PreLoaderDxe/AcpiTableInstaller.c
@@ -1,29 +1,4 @@
-#include <PiDxe.h>
-#include <Uefi.h>
-
-#include <Configuration/Hob.h>
-#include <Pi/PiFirmwareFile.h>
-
-#include <Library/BaseLib.h>
-#include <Library/BmpSupportLib.h>
-#include <Library/DebugLib.h>
-#include <Library/DxeServicesLib.h>
-#include <Library/EfiResetSystemLib.h>
-#include <Library/UefiBootServicesTableLib.h>
-#include <Library/UefiLib.h>
-#include <Library/UefiRuntimeServicesTableLib.h>
-
-#include <IndustryStandard/Acpi.h>
-#include <IndustryStandard/Bmp.h>
-#include <Protocol/AcpiTable.h>
-#include <Protocol/FirmwareVolume2.h>
-#include <Protocol/GraphicsOutput.h>
-
-#include <Library/BgraRgbaConvert.h>
-#include <Library/lodepng.h>
-#include <LittleVgl/core/lv_core/lv_refr.h>
-#include <LittleVgl/core/lvgl.h>
-#include <LittleVgl/lv_conf.h>
+#include "AcpiTableInstaller.h"
 
 STATIC EFI_GUID gAcpiTablesPsci = {
     0x7e374e25,
