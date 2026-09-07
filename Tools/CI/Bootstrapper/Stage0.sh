18c2dc366baadb0f258abc4d150b312c35df1b04
Gustave Monce <gustave.monce@outlook.com>
2026-09-07 11:15:37 +0200

    fix gcc download url

--- Tools/CI/Bootstrapper/Stage0.sh
+++ Tools/CI/Bootstrapper/Stage0.sh
@@ -34,7 +34,7 @@ apt-get install -y powershell
 
 # Linaro Toolchains
 cd /opt
-wget http://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-elf/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf.tar.xz
+wget https://developer.arm.com/-/cdn-downloads/permalink/legacy-linaro-gnu-toolchains/7.5-2019.12/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf.tar.xz
 tar xf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf.tar.xz
 
 # Skip ACPI toolchain (prebuilt tables)
